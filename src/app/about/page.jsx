8aa171bb18f9a6d4e62ff6ce0b75595f1397a3ef
Dan-Victor <victoradesola8@gmail.com>
2025-10-03 10:31:11 +0100

    adjusted footer logo and added about us page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+'use client'
 
-const page = () => {
+import React from 'react'
+import { Typewriter } from 'react-simple-typewriter'
+
+const Page = () => {
+
+  
   return (
-    <div>
-        
+   <div className="relative md:h-[70vh] h-[60vh] bg-[url('/about.jpg')] bg-cover bg-center">
+    {/* Overlay */}
+    <div className="absolute inset-0 bg-[#0F4082]/50"></div>
+
+    {/* Content */}
+    <div className="relative flex flex-col text-center px-5 items-center justify-center h-full">
+      <h1 className="text-white md:text-4xl text-[30px] font-bold">About Us</h1>
+      <p className='text-white md:text-[18px]'><Typewriter
+        words={['Discover our mission, vision, impact, and leadship']}
+        loop={1} // only once
+        cursor={false} // no blinking cursor
+        typeSpeed={40}
+        deleteSpeed={0}
+        delaySpeed={1000}
+      /></p>
     </div>
+  </div>
   )
 }
 
-export default page
+export default Page
--- src/app/component/footer.jsx
+++ src/app/component/footer.jsx
@@ -11,8 +11,8 @@ const footer = () => {
    <footer className='w-full xl:px-10 px-4 md:px-7  '>
     <div className=' grid   grid-cols-1  md:grid-cols-2 lg:grid-cols-3  xl:grid-cols-4 2xl:grid-cols-4 gap-6 lg:gap-8 xl:gap-12 mt-[70px]  justify-center items-start  text-[#4B5564]'>
        <div className=' h-auto '>
-         <Image src='/cclclogo.svg' alt='logo' width={188} height={64}/>
-         <p className='pt-6 h-auto'>Empowering the next generation of leaders to create positive change in their communities and beyond. Join thousands of youth making a difference across Canada</p>
+         <Image src='/cclclogo.svg' alt='logo'width={160} height={64} className='md:w-[160px] md:h-[60px] w-[120px] h-[50px]'/>
+         <p className='pt-3 h-auto'>Empowering the next generation of leaders to create positive change in their communities and beyond. Join thousands of youth making a difference across Canada</p>
         </div>
         <div className='  h-auto'>
             <h1 className='font-semibold text-[#0F4082] text-[18px]'>Programs</h1>
