19b85945b31c9c25269bb2dbd39c47557dad7760
Dan-Victor <victoradesola8@gmail.com>
2025-10-07 16:28:42 +0100

    updated linkedin profile

--- data/socialLinks.jsx
+++ data/socialLinks.jsx
@@ -1,6 +1,8 @@
  export const links =[
         {img:'/facebook.svg', href:'https://www.facebook.com' , alt:'facebook'},
+          {img:'/linkedinwh.svg', href:'https://www.linkedin.com' , alt:'youtube'},
          {img:'/twitter.svg', href:'https://www.twitter.com' , alt:'twitter'},
           {img:'/instagram.svg', href:'https://www.instagram.com' , alt:'instagram'},
            {img:'/youtube.svg', href:'https://www.youtube.com' , alt:'youtube'},
+            
     ]
