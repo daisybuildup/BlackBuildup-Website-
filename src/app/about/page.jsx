cdc995af8ea32aa1476c4b763fc798c8a889af8e
Dan-Victor <victoradesola8@gmail.com>
2025-10-24 19:13:51 +0100

    updated imports

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import Image from 'next/image'
-import AchievementCard from '../component/achievement'
+import AchievementCard from '../component/Achievement'
 import Leadership from '../component/Leadership'
 import TypewriterText from '../component/TypewriterText'
 import  Review  from '../component/review'
