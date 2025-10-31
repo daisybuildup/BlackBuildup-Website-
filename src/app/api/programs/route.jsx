4845c772de9cf0aaf1bb1ae21888f1fbb0673166
Dan-Victor <victoradesola8@gmail.com>
2025-10-31 16:10:16 +0100

    added cache update

--- src/app/api/programs/route.jsx
+++ src/app/api/programs/route.jsx
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'
 import { getPrograms } from '../../../../lib/api'
 
+export const revalidate = 60; 
+
 export async function GET() {
   const program = await getPrograms()
   console.log(program)
