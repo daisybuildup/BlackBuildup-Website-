a04fb52cb54a5144eec6b7cddde2d2ea781e9590
Dan-Victor <victoradesola8@gmail.com>
2025-12-02 17:52:44 +0100

    updated privacy policy ,contact us linkedin profile

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,11 +1,175 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import Email from "./email"
-
+import { useState } from 'react';
 import {links} from '../../../data/socialLinks'
 
+//for contact us modal
+const Contact = ({setIsOpenProp}) => {
+
+  const [showToast, setShowToast] = useState(false);
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+  
+      
+      const formData = new FormData(e.target);
+    
+      const name = formData.get("name");
+       const email = formData.get("email");
+
+      const message = formData.get("message");
+     
+  
+      //  Encode data for mailto link
+      const subject = encodeURIComponent("New Inquiry from Website");
+      const body = encodeURIComponent(
+        `\nname:${name}\nEmail: ${email}\nMessage: ${message}\n from Blackbuildup website`
+      );
+  
+      // mailto link (opens Gmail or default email app)
+      window.location.href = `mailto:info@blackbuildup.com?subject=${subject}&body=${body}`;
+    
+  
+      // Show a quick toast popup
+      setShowToast(true);
+      setTimeout(() => setShowToast(false), 4000);
+     e.target.reset();
+    };
+  
+   
+  return (
+    
+   <div
+      className="fixed  inset-0 w-full h-full bg-black/70 flex justify-center items-center z-[1000] px-6"
+      onClick={() => setIsOpenProp(false)} // click background to close
+    >
+      {/* Modal content */}
+      <div
+        className="text-[#4B5564] rounded-[12px] bg-white py-6 px-4 sm:py-8 sm:px-6 
+                   w-full max-w-md sm:max-w-lg md:max-w-2xl lg:max-w-3xl 
+                   max-h-[70vh] md:max-h-[85vh] overflow-y-auto scrollbar-hide
+                   "
+        onClick={(e) => e.stopPropagation()} // prevent closing when clicking inside
+      >
+        <h1 className="text-[#0F4082] text-left font-bold text-[24px] sm:text-[28px] md:text-[32px]">
+      Contact Us
+        </h1>
+        <p className=" pb-6  text-sm text-left w-[80%] sm:text-base">Send us a message we'll get back to you as soon as possible</p>
+       
+              <form className="space-y-4"   onSubmit={handleSubmit}>
+          
+    
+          <div>
+            <label htmlFor='fullname' className="block mb-1 text-left font-medium"> Name*</label>
+            <input
+              type="text"
+              name="name"
+              required
+              className="w-full border border-[#D9D9D9] rounded-lg p-3"
+
+              placeholder='your name'
+            />
+          </div>
+
+
+          <div>
+            <label htmlFor='email' className="block mb-1 text-left font-medium">Email*</label>
+            <input
+              type="email"
+              name="email"
+              required
+              className="w-full border border-[#D9D9D9] rounded-lg p-3"
+              placeholder='your@email.com'
+            />
+          </div>
+                            
+           <div>
+            <label htmlFor='message' className="block mb-1 text-left font-medium">Message*</label>
+            <textarea 
+              name="message"
+              placeholder= "How can we help you?"
+              rows="3"
+              required
+              className="w-full border border-[#D9D9D9] rounded-lg p-3"
+            ></textarea>
+          </div>
+
+         
+                
+               {/*  Toast notification */}
+            {showToast && (
+        <div className="fixed top-5 left-1/2 transform -translate-x-1/2 bg-green-600 text-white px-5 py-3 rounded-lg shadow-lg text-sm z-[1100]">
+            Email draft opened successfully
+              </div>
+          )}
+
+              <div className='flex gap-6'>
+          <button
+            type="submit"
+            className="w-[100%] bg-[#0F4082] text-white py-3 rounded-2xl hover:bg-[#1051a6] cursor-pointer transition"
+          
+          >
+            Send Message
+          </button>
+         
+          </div>
+          
+        </form>
+  
+      </div>
+    </div>
+  )
+}
+
+
+
+const PrivacyPolicy = ({setIsOpenProp}) => {
+
+    
+  return (
+    
+   <div
+      className="fixed  inset-0 w-full h-full bg-black/70 flex justify-center items-center z-[1000] px-6"
+      onClick={() => setIsOpenProp(false)} // click background to close
+    >
+      {/* Modal content */}
+      <div
+        className="text-[#4B5564] rounded-[12px] bg-white py-6 px-4 sm:py-8 sm:px-6 
+                   w-full max-w-md sm:max-w-lg md:max-w-2xl lg:max-w-3xl 
+                   max-h-[70vh] md:max-h-[85vh] overflow-y-auto scrollbar-hide
+                   "
+        onClick={(e) => e.stopPropagation()} // prevent closing when clicking inside
+      >
+        <h1 className="text-[#0F4082] text-left font-bold text-[24px] sm:text-[28px] md:text-[32px]">
+      Privacy Policy
+        </h1>
+        <p className=" pb-6  text-sm text-left md:w-[80%] sm:text-base">Our commitment to protecting your privacy and personal information</p>
+         <p className=" pb-6  text-sm text-left md:w-[90%] sm:text-base">At Black Buildup, we take your privacy seriously. We are committed to protecting your personal information and being transparent about how we collect, use and share your data.</p>
+       <div className='bg-[#32609c1a] rounded-lg p-6 md:grid grid-cols-2  md:gap-10 gap-3 '>
+     <div className='flex gap-1.5  items-center'>
+      <Image src='/document.svg' alt='privacy' width={48} height={48}/>
+      <div className='flex flex-col gap-2.5'>
+      <h2 className="text-[#0F4082] font-semibold pt-2 text-xl">Full Privacy Policy Document</h2>
+      <p className=" pb-6  text-sm text-left  sm:text-base">Complete details about our privacy practices</p>
+      </div>
+      
+     </div>
+      <button className='bg-white rounded-2xl border border-[#0F4082] cursor-pointer text-[#0F4082] font-medium  hover:text-white hover:bg-[#0F4082] py-3 px-3.5'><a href="/privacypolicy.pdf"  download>View PDF</a></button>
+        </div>
+    <p className=" pb-6  text-sm text-left  sm:text-base text-black">For questions about our privacy policy, please contact us at privacy@black.ca</p>
+      </div>
+    </div>
+  )
+}
+
+
+
 const footer = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isPrivacyOpen, setIsPrivacyOpen] = useState(false);
    
   return (
    <footer className='w-full xl:px-10 px-4 md:px-7  '>
@@ -37,8 +201,14 @@ const footer = () => {
         <div className=' h-auto'>
             <h1 className='font-semibold text-[#0F4082] text-[18px]'>Support</h1>
             <ul className="pt-4 flex flex-col gap-4">
-                <li>Contact Us</li>
-                <li>Privacy Policy</li>
+                <li onClick={()=>setIsOpen(true)} className='cursor-pointer'>Contact Us</li>
+                {
+                  isOpen && <Contact setIsOpenProp={setIsOpen}  />
+                }
+                <li onClick={()=>setIsPrivacyOpen(true)}  className='cursor-pointer'>Privacy Policy</li>
+                {
+                  isPrivacyOpen && <PrivacyPolicy setIsOpenProp={setIsPrivacyOpen}  />
+                }
                 {/*<li>FAQ</li>
                 <li>Help Center</li>
                
